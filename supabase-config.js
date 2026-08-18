519b4bd547fb05a1384d75bd8737af8fde3bc45e
ElRosillo <luisrosillo.mon@gmail.com>
2026-08-17 18:37:54 -0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 // Nunca coloques aquí la service_role key.
 window.SUPABASE_CONFIG = {
   url: 'https://giuxyimxvivnkofianwm.supabase.co',
-  anonKey: 'sb_publishable_zpbPj-7pAiHkPK5Ts3nhvA_A6j5AQ29'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpdXh5aW14dml2bmtvZmlhbndtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwMDgyNDcsImV4cCI6MjEwMjU4NDI0N30.ShWY4cI1NdsFkjJedsGaqmPQue_-jOl-RyuQqOD684E'
 };
