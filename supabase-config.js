03354fb971addeaa9a58fa4991735ac3dcf4fa6e
ElRosillo <luisrosillo.mon@gmail.com>
2026-08-17 18:34:38 -0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // Estas dos claves son públicas y deben corresponder a tu proyecto Supabase.
 // Nunca coloques aquí la service_role key.
 window.SUPABASE_CONFIG = {
-  url: 'PEGA_AQUI_TU_PROJECT_URL',
-  anonKey: 'PEGA_AQUI_TU_PUBLISHABLE_O_ANON_KEY'
+  url: 'https://giuxyimxvivnkofianwm.supabase.co',
+  anonKey: 'sb_publishable_zpbPj-7pAiHkPK5Ts3nhvA_A6j5AQ29'
 };
